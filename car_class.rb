a879486977caa5ec75d6d000269aca9cd3d04f49
PamBWillenz <pam@positek.net>
2024-12-06 09:21:11 -0500

    more on classes

--- car_class.rb
+++ car_class.rb
@@ -1,41 +1,85 @@
-class MyCar
+module Towable
+  def can_tow?(pounds)
+    pounds < 2000 ? true : false
+  end
+end
+
+class Vehicle
   attr_accessor :color
-  attr_reader :year
+  attr_reader :year, :model
+  @@number_of_vehicles = 0
+
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+
+  def self.number_of_vehicles
+    puts "Number of vehicles created: #{@@number_of_vehicles}"
+  end
 
   def initialize(year, color, model)
     @year = year
     @color = color
     @model = model
     @speed = 0
+    @@number_of_vehicles += 1
   end
+
   def speed_up(num)
     @speed += num
     puts "You are now going #{@speed} mph."
   end
+
   def brake(num)
     @speed -= num
     puts "You are now going #{@speed} mph."
   end
+
   def shut_off
     @speed = 0
     puts "Car is shut off"
   end
+
   def change_color(c)
     @color = c
     puts "The car color is now #{c}"
   end
-  def year=(y)
-    @year = y
-  end
+
   def spray_paint(color)
     self.color = color
     puts "The car color is now #{color}"
   end
-  def self.gas_mileage(gallons, miles)
-    puts "#{miles / gallons} miles per gallon of gas"
-  end
+
   def to_s 
-    "My car is a #{color}, #{year}, #{@model}!"
+    "My vehicle is a #{color}, #{year}, #{@model}!"
+  end
+
+  def age
+    "Your #{self.model} is #{years_old} years old."
+  end
+
+  private
+
+  def years_old
+    Time.now.year - self.year.to_i
+  end
+end
+
+class MyCar < Vehicle
+  NUMBER_OF_DOORS = 4
+  
+  def to_s
+    "My car is a #{self.color}, #{self.year}, #{self.model}!"
+  end
+end
+
+class MyTruck < Vehicle
+  include Towable
+  
+  NUMBER_OF_DOORS = 2
+  
+  def to_s
+    "My truck is a #{self.color}, #{self.year}, #{self.model}!"
   end
 end
 
@@ -52,3 +96,10 @@ puts car.to_s
 
 my_car = MyCar.new("2010", "Ford", "Focus")
 puts my_car
+
+Vehicle.number_of_vehicles
+puts MyCar.ancestors
+puts MyTruck.ancestors
+puts Vehicle.ancestors
+
+puts car.age
---
+++ student_class.rb
@@ -0,0 +1,35 @@
+class Student
+
+  def initialize(name, grade) 
+    @name = name
+    @grade = grade
+  end
+
+  def better_grade_than?(other_student)
+    grade > other_student.grade
+  end
+
+  protected
+
+  def grade
+    @grade
+  end
+end
+
+class Person
+  def initialize(name)
+    @name = name
+  end
+
+  def hi
+    puts "Hi, my name is #{@name}"
+  end
+end
+
+bob = Person.new("Bob")
+bob.hi 
+
+
+joe = Student.new("Joe", 90)
+bob = Student.new("Bob", 84)
+puts "Well Done!" if joe.better_grade_than?(bob)
